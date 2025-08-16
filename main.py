f6560f2d57cef4f063e1e2998accbe1ab9e5801d
Victor Bussiki <victorbussiki@gmail.com>
2025-08-16 11:32:36 -0300

    (build): add dev libraries

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
-    print("Hello from urlshortener!")
+	print('Hello from urlshortener!')
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+	main()
+
+poop = 1
+
+poop = 'a'
